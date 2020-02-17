a48c84ec3ea705928670071fedfd3f7700a3a507
asalinas9 <salinas99@live.com>
2020-02-17 09:31:10 -0600

    updated Psychic game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,42 +4,51 @@ var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
-var guessesSoFar = []
+var guessedSoFar = []
 
 
 var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 console.log(computerChoice);
  
 function reset(){
-    guessesLeft = 10;
-    guessesSoFar = [];
+    guessesLeft = 9;
+    guessedSoFar = [];
 }
 
 document.onkeyup = function(event) {
     var userGuess = event.key;
+    guessedSoFar.push(userGuess);
    
 
     if(userGuess === computerChoice){
         wins++;
-        document.getElementById("wins").innerHTML = wins;
-        alert("you win!")
-        guessesSoFar.push(userGuess);
-        reset();
+        guessesLeft = 10;
+        guessedSoFar = []
+        
+        alert("you win")
         
     }
 
     else(userGuess !== computerChoice)
         guessesLeft--;
-        document.getElementById("guessesLeft").innerHTML = guessesLeft;
         console.log(userGuess);
     
     
     }
     if( guessesLeft === 0){
         losses++;
-        document.getElementById("header").innerHTML = "Please try again"
-        reset()
+        guessesLeft = 10;
+        guessedSoFar = []
+
+        alert("sorry, try again!");
 
     }
+
+    document.getElementById("wins").innerHTML = "Wins: " + wins;
+    document.getElementById("losses").innerHTML = "Losses: " + losses;
+    document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+    document.getElementById("guessedSoFar").innerHTML = "guessed So Far: " + guessedSoFar;
+
+
 
 
